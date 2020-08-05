0f4551a643518098a861961c6336cf026f273f08
presida <53336246+Presida@users.noreply.github.com>
2020-08-05 11:04:59 +0700

    CRUD for Category

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,10 @@
 <?php
 
 Route::apiResource('/question', 'QuestionController');
+Route::apiResource('/category', 'CategoryController');
+
+Route::fallback(function() {
+    return response()->json([
+        'message' => 'Page Not Found.'
+    ], 404);
+});
