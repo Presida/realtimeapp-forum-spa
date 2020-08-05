e717c5df7bd6d33cbeb651ef89c826e634c95d37
presida <53336246+Presida@users.noreply.github.com>
2020-08-05 23:17:06 +0700

    JWT Package

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,16 @@ Route::apiResource('/question/{question}/reply', 'ReplyController');
 Route::post('/like/{reply}', 'LikeController@likeIt');
 Route::delete('/like/{reply}', 'LikeController@unLikeIt');
 
-Route::fallback(function() {
-    return response()->json([
-        'message' => 'Page Not Found.'
-    ], 404);
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
 });
