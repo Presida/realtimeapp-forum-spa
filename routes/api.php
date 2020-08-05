28d8b52b7022da5a15946abf44ebfd50e74d2bfe
presida <53336246+Presida@users.noreply.github.com>
2020-08-05 19:36:18 +0700

    Like and Dislike system for reply

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 Route::apiResource('/question/{question}/reply', 'ReplyController');
+Route::post('/like/{reply}', 'LikeController@likeIt');
+Route::delete('/like/{reply}', 'LikeController@unLikeIt');
 
 Route::fallback(function() {
     return response()->json([
